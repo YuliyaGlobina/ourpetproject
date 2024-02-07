7ec904ad1d8e1283ba73e93e4e691f36377a6547
Yuliya Globina <y.globina@yandex.ru>
2024-02-07 13:51:31 +0300

    переход на интерфейс JPA Repository

--- src/main/java/com/ourpetproject/ourpetproject/service/EmployeeServiceImpl.java
+++ src/main/java/com/ourpetproject/ourpetproject/service/EmployeeServiceImpl.java
@@ -1,40 +1,42 @@
 package com.ourpetproject.ourpetproject.service;
 
 
-import com.ourpetproject.ourpetproject.dao.EmployeeDAO;
+import com.ourpetproject.ourpetproject.dao.EmployeeRepository;
 import com.ourpetproject.ourpetproject.entity.Employee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeServiceImpl implements EmployeeService{
     @Autowired
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
 
     @Override
-    @Transactional
     public List<Employee> getAllEmployees() {
-        return employeeDAO.getAllEmployees();
+        return employeeRepository.findAll();
     }
 
     @Override
-    @Transactional
     public void saveEmployee(Employee employee) {
-        employeeDAO.saveEmployee(employee);
+        employeeRepository.save(employee);
     }
 
     @Override
-    @Transactional
     public Employee getEmployee(int id) {
-        return employeeDAO.getEmployee(id);
+        Employee employee = null;
+        Optional<Employee> emp = employeeRepository.findById(id);
+        if(emp.isPresent()){
+            employee = emp.get();
+        }
+        return employee;
     }
 
     @Override
-    @Transactional
     public void deleteEmployee(int id) {
-        employeeDAO.deleteEmployee(id);
+        employeeRepository.deleteById(id);
 
     }
 }
